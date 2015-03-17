3d8fa8963243b0118f5a29d55b7dac0384bf6281
Mariane Tiemi <marianetiemi@gmail.com>
2015-03-16 23:28:42 -0300

    EnquirerAnimals atualizado, enquirerMaze implementado

--- src/pt/c02classes/s01knowledge/s02app/actors/EnquirerAnimals.java
+++ src/pt/c02classes/s01knowledge/s02app/actors/EnquirerAnimals.java
@@ -6,37 +6,57 @@ import pt.c02classes.s01knowledge.s01base.inter.IDeclaracao;
 import pt.c02classes.s01knowledge.s01base.inter.IEnquirer;
 import pt.c02classes.s01knowledge.s01base.inter.IObjetoConhecimento;
 import pt.c02classes.s01knowledge.s01base.inter.IResponder;
+import java.util.Map;
+import java.util.HashMap;
 
 public class EnquirerAnimals implements IEnquirer {
 
 	IResponder responder;
+	IObjetoConhecimento obj;
+	String[] vetorAnimal; /* vetor que guarda a lista de animais */
+	Map <String,String> perguntaFeita = new HashMap <String,String>(); /* cria um hashtable para armazenar as perguntas e respostas */
+	int i = 0;
 	
 	public void connect(IResponder responder) {
 		this.responder = responder;
 	}
+
 	
 	public boolean discover() {
-        IBaseConhecimento bc = new BaseConhecimento();
-        IObjetoConhecimento obj;
-		
-		bc.setScenario("animals");
-        obj = bc.recuperaObjeto("tiranossauro");
-
-		IDeclaracao decl = obj.primeira();
-		
-        boolean animalEsperado = true;
-		while (decl != null && animalEsperado) {
-			String pergunta = decl.getPropriedade();
-			String respostaEsperada = decl.getValor();
+		 	IBaseConhecimento bc = new BaseConhecimento();
+		 	bc.setScenario("animals");
+			vetorAnimal = bc.listaNomes(); /* coloca todos os animais disponiveis no vetor */
+			obj = bc.recuperaObjeto(vetorAnimal[i]); 
 			
-			String resposta = responder.ask(pergunta);
-			if (resposta.equalsIgnoreCase(respostaEsperada))
-				decl = obj.proxima();
-			else
-				animalEsperado = false;
-		}
-		
-		boolean acertei = responder.finalAnswer("tiranossauro");
+			IDeclaracao decl = obj.primeira();
+			
+			while (decl != null) 
+			{
+				String pergunta = decl.getPropriedade();
+				String respostaEsperada = decl.getValor();
+				String resposta;
+				
+				if (perguntaFeita.containsKey(pergunta)) /* se o hashmap estiver com alguma pergunta feita já */
+				{
+					resposta = perguntaFeita.get(pergunta); /* vê a resposta que foi dada anterior e assim não precisa perguntar de novo */
+				}else /* se a pergunta nao foi feita ainda */
+				{
+					resposta = responder.ask(pergunta);
+					perguntaFeita.put(pergunta, resposta); /* coloca a pergunta e resposta na hashtable */
+				}
+						
+				if (resposta.equalsIgnoreCase(respostaEsperada)) /* compara a resposta dada e a esperada */
+				{
+					decl = obj.proxima();
+				}else /* se nao for a resposta esperada, vai para o proximo animal */
+				{
+					i++;
+					obj = bc.recuperaObjeto(vetorAnimal[i]);
+					decl = obj.primeira();
+				}	
+			}
+			
+			boolean acertei = responder.finalAnswer(vetorAnimal[i]);
 		
 		if (acertei)
 			System.out.println("Oba! Acertei!");
--- src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
+++ src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
@@ -4,42 +4,131 @@ import java.util.Scanner;
 
 import pt.c02classes.s01knowledge.s01base.inter.IEnquirer;
 import pt.c02classes.s01knowledge.s01base.inter.IResponder;
+import java.util.Stack;
 
 public class EnquirerMaze implements IEnquirer {
 
 	IResponder responder;
-	
+	Stack <String> stackPosition = new Stack <String>(); /* usando pilha para armazenar o caminho feito no labirinto */
+	String[] direction = new String[]{"norte", "leste", "sul", "oeste"};
+
 	public void connect(IResponder responder) {
 		this.responder = responder;
 	}
 	
-	public boolean discover() {
-		Scanner scanner = new Scanner(System.in);
+	public boolean verifyMove(String direction) /* verifica se o proximo movimento eh valido */
+	{
+		String position;
+		position = responder.ask(direction);
 		
-		System.out.print("(P)ergunta, (M)ovimento ou (F)im? ");
-		String tipo = scanner.nextLine();
-		while (!tipo.equalsIgnoreCase("F")) {
-		   System.out.print("  --> ");
-		   String pc = scanner.nextLine();
-		   switch (tipo.toUpperCase()) {
-		      case "P": String resposta = responder.ask(pc);
-		                System.out.println("  Resposta: " + resposta);
-		                break;
-		      case "M": boolean moveu = responder.move(pc);
-		                System.out.println((moveu)?"  Movimento executado!":"Não é possível mover");
-		                break;
-		   }
-			System.out.print("(P)ergunta, (M)ovimento ou (F)im? ");
-			tipo = scanner.nextLine();
+		if((position.equalsIgnoreCase("saida"))|| (position.equalsIgnoreCase("passagem"))) /* se for um movi */
+		{
+			if(!(stackPosition.empty()) ) /* se a pilha estiver com elemento */
+			{	
+				/* para nao ter loop e ficar movendo repetidamente */
+				if((direction == "norte" && stackPosition.peek() != "sul")||(direction == "sul" && stackPosition.peek() != "norte")||(direction == "leste" && stackPosition.peek() != "oeste")||(direction == "oeste" && stackPosition.peek() != "leste"))
+				{
+					return true;
+				}		
+			}else if(stackPosition.empty()) /* se a pilha estiver vazia, pode fazer tal movimento pois n teve nenhum movimento antes */
+			{
+				return true;
+			}	
 		}
 		
-		if (responder.finalAnswer("cheguei"))
+		return false;
+	}
+	
+	public boolean solutionMaze() /* resolve o maze, ou seja, encontra a saida do labirinto */
+	{
+		boolean solution = false;
+		
+		if((responder.ask("aqui")).equalsIgnoreCase("saida")) /* se achar a saida do labirinto*/
+		{
+			return true;
+		}
+		
+		if(verifyMove(direction[0]) && !solution) /* se for possivel ir para o norte */
+		{
+			responder.move(direction[0]);
+			stackPosition.push(direction[0]);
+			System.out.println("Moveu para Norte!");
+			
+			if(solutionMaze())
+			{
+				return true;
+			}else
+			{					
+				stackPosition.pop();
+				responder.move(direction[2]);
+				System.out.println("Voltou caminho e foi para Sul!");
+			}
+		}
+		
+		if(verifyMove(direction[1]) && !solution) /* se for possivel ir para o leste */
+		{
+			responder.move(direction[1]);
+			stackPosition.push(direction[1]);
+			System.out.println("Moveu para Leste!");
+			
+			if(solutionMaze())
+			{
+				return true;
+			}else
+			{
+				stackPosition.pop();
+				responder.move(direction[3]);
+				System.out.println("Voltou caminho e foi para Oeste!");
+			}
+		}
+		
+		if(verifyMove(direction[2]) && !solution ) /* se for possivel ir para o sul*/
+		{
+			responder.move(direction[2]);
+			stackPosition.push(direction[2]);
+			System.out.println("Moveu para Sul!");
+			
+			if(solutionMaze())
+			{
+				return true;
+			}else
+			{
+				stackPosition.pop();
+				responder.move(direction[0]);
+				System.out.println("Voltou caminho e foi para Norte!");
+			}
+		}
+		
+		if(verifyMove(direction[3]) && !solution) /* se for possivel ir para o oeste */
+		{
+			responder.move(direction[3]);
+			stackPosition.push(direction[3]);
+			System.out.println("Moveu para Oeste!");
+			
+			if(solutionMaze())
+			{
+				return true;
+			}else
+			{
+				stackPosition.pop();
+				responder.move(direction[1]);
+				System.out.println("Voltou caminho e foi para Leste!");
+			}
+		}
+		return solution;
+	}
+
+	public boolean discover() 
+	{
+		if(solutionMaze()) /* encontrou saida */
+		{
 			System.out.println("Você encontrou a saida!");
-		else
-			System.out.println("Fuém fuém fuém!");
-		
-		scanner.close();
-		
+			
+		}else 
+		{
+			System.out.println("Fuém fuém fuém!");	
+			
+		}
 		return true;
 	}
 	
